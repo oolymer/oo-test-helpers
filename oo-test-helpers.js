7c76381a8b4ff4d6a561193d8802db860b477787
Benjamin Gudehus <hastebrot@gmail.com>
2018-04-07 20:25:30 +0200

    (refactor) Rename to setup.wait() and test.wait().

--- oo-test-helpers.js
+++ oo-test-helpers.js
@@ -1,19 +1,9 @@
 (function(global) {
-  // blocks the test suite run after Mocha's setup() was called.
-  // useful to explore the current state of the user interface.
-  global.setupBlocked = function setupBlocked(callback) {
-    global.setup(function(done) {
-      this.enableTimeouts(false)
-      callback(done)
-    })
-  }
-
+  const Mocha = window.Mocha
   const interfaceExtensions = []
 
   function extendMochaInterfaces(helperFactory) {
     interfaceExtensions.push(() => {
-      const Mocha = window.Mocha
-
       Object.keys(Mocha.interfaces).forEach(interfaceName => {
         const baseInterface = Mocha.interfaces[interfaceName]
 
@@ -33,15 +23,27 @@
     })
   }
 
-  extendMochaInterfaces(context => {
-    context.setup.blocked = function blocked(callback) {
-      context.setup(function(done) {
-        this.enableTimeouts(false)
-        callback(done)
-      })
+  extendMochaInterfaces((context, interfaceName) => {
+    if (interfaceName === "tdd") {
+      // blocks the test suite run after Mocha's setup() was called.
+      // useful to explore the current state of the user interface.
+      context.setup.wait = function wait(callback) {
+        context.setup(function(done) {
+          this.enableTimeouts(false)
+          callback(done)
+        })
+      }
+
+      // blocks the test suite run after Mocha's test() was called.
+      // useful to explore the current state of the user interface.
+      context.test.wait = function wait(name, callback) {
+        context.test(name, function(done) {
+          this.enableTimeouts(false)
+          callback(done)
+        })
+      }
     }
   })
 
   applyMochaExtensions()
-
 })(this)
